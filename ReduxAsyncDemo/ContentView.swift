423161bfe9991cb45f0a32ecfc41ecd4ff4d0331
jyrnan <yoeking@gmail.com>
2022-04-29 14:09:11 +0800

    Initial Commit

---
+++ ReduxAsyncDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReduxAsyncDemo
+//
+//  Created by Yong Jin on 2022/4/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReduxAsyncDemo/ReduxAsyncDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxAsyncDemoApp.swift
+//  ReduxAsyncDemo
+//
+//  Created by Yong Jin on 2022/4/29.
+//
+
+import SwiftUI
+
+@main
+struct ReduxAsyncDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
